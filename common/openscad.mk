342b0025197c58e53f5ba75293aa461a642298a5
Matúš Horváth <matus.horvath@gmail.com>
2026-01-22 00:28:55 +0100

    Improve WSL detection

--- common/openscad.mk
+++ common/openscad.mk
@@ -1,6 +1,6 @@
 # Detect OpenSCAD location
 
-ifdef WSLENV
+ifneq ($(shell wslinfo --version),)
 	OPENSCAD="/mnt/c/Program Files/OpenSCAD/openscad.exe"
 else ifeq ($(shell uname -s),Darwin)
 	OPENSCAD="/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD"
